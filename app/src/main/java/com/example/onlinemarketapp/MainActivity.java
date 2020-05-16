a80fb02b412d2ac4d1ae32c7a5bd7f3386afb53c
Petra Horvath <petra.horvath99@gmail.com>
2020-05-16 17:11:30 +0300

    started implementing the main activity

--- app/src/main/java/com/example/onlinemarketapp/MainActivity.java
+++ app/src/main/java/com/example/onlinemarketapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.onlinemarketapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //We connect the buttons in main activity to the once from the xml
+        loginButton = (Button) findViewById(R.id.main_login_button);
+        registerButton = (Button) findViewById(R.id.main_register_utton);
+
     }
 }
