150ccf4af531c8b0b4c6b3c8eb5a48519b1c2b67
Paul Carabas <carabas_paul@yahoo.com>
2020-05-17 02:07:38 +0300

    Created Register UI

--- app/src/main/java/com/example/onlinemarketapp/LoginActivity.java
+++ app/src/main/java/com/example/onlinemarketapp/LoginActivity.java
@@ -1,24 +1,10 @@
 package com.example.onlinemarketapp;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.onlinemarketapp.Model.Buyer;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,65 +20,65 @@ public class LoginActivity extends AppCompatActivity {
         inputEmail = (EditText) findViewById(R.id.login_email_input);
         inputPassword = (EditText) findViewById(R.id.login_password_input);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loginUser();
-            }
-        });
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                loginUser();
+//            }
+//        });
     }
 
-    private void loginUser() {
-        String email = inputEmail.getText().toString();
-        String password = inputPassword.getText().toString();
-
-         if(TextUtils.isEmpty(email)){
-            Toast.makeText(this, "Please write your email", Toast.LENGTH_SHORT).show();
-        }
-        else if(TextUtils.isEmpty(password)){
-            Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
-        }
-        else{
-
-            login(email, password);
-        }
-    }
-
-    private void login(final String email, final String password) {
-        final DatabaseReference myDatabaseReference;
-        myDatabaseReference = FirebaseDatabase.getInstance().getReference();
-
-        myDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.child("Buyers").child(email).exists()){
-                    Buyer buyerData = dataSnapshot.child("Buyers").child(email).getValue(Buyer.class);
-
-                    final String salt = buyerData.getSalt();
-                    final String encryptedPassword = Encryption.generateSecurePassword(password, salt);
-
-                    if(buyerData.getEmail().equals(email)){
-                        if (Encryption.verifyUserPassword(password, encryptedPassword, salt)) {
-                            Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
-
-                            Intent intent = new Intent(LoginActivity.this, BrowseActivity.class);
-                            startActivity(intent);
-                        }
-                        else {
-                            Toast.makeText(LoginActivity.this, "Password is incorrect, try again!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-                else{
-                    Toast.makeText(LoginActivity.this, "There is no account with the inputted email address", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
+//    private void loginUser() {
+//        String email = inputEmail.getText().toString();
+//        String password = inputPassword.getText().toString();
+//
+//         if(TextUtils.isEmpty(email)){
+//            Toast.makeText(this, "Please write your email", Toast.LENGTH_SHORT).show();
+//        }
+//        else if(TextUtils.isEmpty(password)){
+//            Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
+//        }
+//        else{
+//
+//            login(email, password);
+//        }
+//    }
+//
+//    private void login(final String email, final String password) {
+//        final DatabaseReference myDatabaseReference;
+//        myDatabaseReference = FirebaseDatabase.getInstance().getReference();
+//
+//        myDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @RequiresApi(api = Build.VERSION_CODES.O)
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if(dataSnapshot.child("Buyers").child(email).exists()){
+//                    Buyer buyerData = dataSnapshot.child("Buyers").child(email).getValue(Buyer.class);
+//
+//                    final String salt = buyerData.getSalt();
+//                    final String encryptedPassword = Encryption.generateSecurePassword(password, salt);
+//
+//                    if(buyerData.getEmail().equals(email)){
+//                        if (Encryption.verifyUserPassword(password, encryptedPassword, salt)) {
+//                            Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+//
+//                            Intent intent = new Intent(LoginActivity.this, BrowseActivity.class);
+//                            startActivity(intent);
+//                        }
+//                        else {
+//                            Toast.makeText(LoginActivity.this, "Password is incorrect, try again!", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                }
+//                else{
+//                    Toast.makeText(LoginActivity.this, "There is no account with the inputted email address", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/example/onlinemarketapp/MainActivity.java
+++ app/src/main/java/com/example/onlinemarketapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.onlinemarketapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,29 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //We connect the buttons in main activity to the once from the xml
-        loginButton = (Button) findViewById(R.id.main_login_button);
-        registerButton = (Button) findViewById(R.id.main_register_button);
-        browseButton = (Button) findViewById(R.id.main_browse_btn);
+        loginButton = findViewById(R.id.main_login_btn);
+        registerButton = findViewById(R.id.main_join_now_btn);
+        browseButton = findViewById(R.id.main_browse_btn);
 
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        registerButton.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        browseButton.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, BrowseActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        browseButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BrowseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
