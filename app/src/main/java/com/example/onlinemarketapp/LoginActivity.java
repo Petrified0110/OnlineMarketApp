0677868139c3cf3eaacfafc1424da028ebd33311
Petra Horvath <petra.horvath99@gmail.com>
2020-05-16 20:48:30 +0300

    Browse Activity created

--- app/src/main/java/com/example/onlinemarketapp/LoginActivity.java
+++ app/src/main/java/com/example/onlinemarketapp/LoginActivity.java
@@ -7,10 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.onlinemarketapp.Model.Buyer;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -50,10 +55,32 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void login(String email, String password) {
+    private void login(final String email, final String password) {
         final DatabaseReference myDatabaseReference;
         myDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
+        myDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("Buyers").child(email).exists()){
+                    Buyer buyerData = dataSnapshot.child("Buyers").child(email).getValue(Buyer.class);
 
+                    if(buyerData.getEmail().equals(email)){
+                        if (buyerData.getPassword().equals(password)) {
+
+                            Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "There is no account with the inputted email address", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
