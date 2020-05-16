820ecb10201e8f000a3a8b738dfc39c671c72b52
Petra Horvath <petra.horvath99@gmail.com>
2020-05-16 17:27:22 +0300

    started implementing the Login and Register Activity

--- app/src/main/java/com/example/onlinemarketapp/MainActivity.java
+++ app/src/main/java/com/example/onlinemarketapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.onlinemarketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.main_login_button);
         registerButton = (Button) findViewById(R.id.main_register_utton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
